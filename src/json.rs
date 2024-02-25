e6f7423a26ec82a156f9e5c6c8667fbe8d424137
Andrzej Głuszak <andrzej.gluszak@jetbrains.com>
2024-02-25 01:06:21 +0100

    Fix

--- src/json.rs
+++ src/json.rs
@@ -5,7 +5,7 @@ mod date_serde {
     use chrono::NaiveDate;
     use serde::{Deserialize, Deserializer, Serializer};
 
-    const FORMAT: &str = "%d.%m.%Y";
+    const FORMAT: &str = "%Y.%m.%d";
 
     pub fn serialize<S>(date: &NaiveDate, serializer: S) -> Result<S::Ok, S::Error>
     where
@@ -21,5 +21,26 @@ mod date_serde {
     {
         let s = String::deserialize(deserializer)?;
         NaiveDate::parse_from_str(&s, FORMAT).map_err(serde::de::Error::custom)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use chrono::NaiveDate;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize, PartialEq, Debug)]
+    #[repr(transparent)]
+    struct TestStruct(#[serde(with = "date_serde")] NaiveDate);
+
+    #[test]
+    fn date_serde_works() {
+        let date = TestStruct(NaiveDate::from_ymd_opt(2021, 1, 30).unwrap());
+        let s = serde_json::to_string(&date).unwrap();
+        assert_eq!(s, "\"2021.01.30\"");
+
+        let parsed_date: TestStruct = serde_json::from_str(&s).unwrap();
+        assert_eq!(parsed_date, date);
     }
 }
